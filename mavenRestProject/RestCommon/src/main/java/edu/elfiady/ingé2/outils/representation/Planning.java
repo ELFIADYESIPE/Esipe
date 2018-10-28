d8331d45bd011fafb058a9b14451d4e2716ecd86
unknown <damien.baratonman@gmail.com>
2018-10-28 15:49:18 +0100

    Revert "Le code ne compile pas"
    This reverts commit 2a58a7911e701572aea391c8cb7ef2e5ed3f08e5.

--- mavenRestProject/RestCommon/src/main/java/edu/elfiady/ingé2/outils/representation/Planning.java
+++ mavenRestProject/RestCommon/src/main/java/edu/elfiady/ingé2/outils/representation/Planning.java
@@ -9,7 +9,7 @@ public class Planning {
 	}
 	
 	public void modifierPlanning() {
-		setId(this.id+1+CECI EST UNE deuxième ERREUR DE COMPILATION);
+		setId(this.id+1+CECI EST UNE ERREUR DE COMPILATION);
 	}
 	
 	public void setId(int id) {
